e4f62ab9a8eafe0e30d50629c300fad58513b291
Matthew O'Dea <matthew.odea@gmail.com>
2018-03-13 12:00:22 +0800

    Initial Commit
    Transferring from my old account.

---
+++ OattsWebsite/Pages/Contact.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OattsWebsite.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Get in touch with Oatts!";
+        }
+    }
+}
